51491e0059e8d476679c5eb9f48801abe69f8896
SawyerPorte <sporte@ucsc.edu>
2025-09-30 12:47:39 -0700

    slash

--- TDDBackendStats/Program.cs
+++ TDDBackendStats/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("https://card-game-stats-front-end.vercel.app/") // replace with your deployed frontend URL
+            policy.WithOrigins("http://localhost:3000","https://card-game-stats-front-end.vercel.app") // replace with your deployed frontend URL
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
